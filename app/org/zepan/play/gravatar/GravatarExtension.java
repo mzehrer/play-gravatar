503f1fa8cb57567258edfc4f332a058db2c2764c
Michael Zehrer <zehrer@zepan.net>
2010-12-21 15:42:57 +0100

    Default values for tag options

--- app/org/zepan/play/gravatar/GravatarExtension.java
+++ app/org/zepan/play/gravatar/GravatarExtension.java
@@ -5,15 +5,22 @@ import play.templates.JavaExtensions;
 
 public class GravatarExtension extends JavaExtensions {
 
-    public static String gravatarUrl(String email, String size, String d) {
+    public static String gravatarUrl(String email, String s, String d) {
 
-	if (d == null)
-		d="mm";
+	StringBuffer gravReq = new StringBuffer();
 
         String gravatarImageBaseUrl = "http://www.gravatar.com/avatar/";
         String md5Email = MD5Util.md5Hex(email);
+	gravReq.append(gravatarImageBaseUrl);
+	gravReq.append(md5Email + "?");
 
-        return gravatarImageBaseUrl + md5Email + "?s=" + size + "&d=" + d;
+	if (s != null && Integer.parseInt(s) > 0 && Integer.parseInt(s) < 513)
+		gravReq.append("s=" + s + "&");
+
+	if (d != null && d.matches("mm|identicon|monsterid|wavatar|retro"))
+		gravReq.append("d=" + d + "&");
+
+        return gravReq.toString();
     }
 
 }
